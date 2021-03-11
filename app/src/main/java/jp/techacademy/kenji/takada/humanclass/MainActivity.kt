fce206daae714552ef341ba38344000410c9d1d4
FALCONKT <falcon9628@yahoo.co.jp>
2021-03-11 22:55:44 +0900

    Human_Class設定

--- app/src/main/java/jp/techacademy/kenji/takada/humanclass/MainActivity.kt
+++ app/src/main/java/jp/techacademy/kenji/takada/humanclass/MainActivity.kt
@@ -10,12 +10,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //Instance化①　　人名は文字列　年齢は数値　趣味は文字列
-        val man1 = Human("人間１",25,"Sky")
+        val man1 = Human("人間１",29,"Sky")
         man1.say()
         man1.think()
 
         //Instance化②　　人名は文字列　年齢は数値　趣味は文字列
-        val man2 = Human("人間２",35,"絵画")
+        val man2 = Human("人間２",39,"絵画")
         man2.say()
         man2.think()
 
